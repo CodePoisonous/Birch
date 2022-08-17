ddb64999d27b846ed6c5db8f617a07b49e1ebae0
chuyan1 <379482455@qq.com>
2022-08-17 21:47:37 +0800

    在窗口中绘制首个三角形

--- Birch/src/Platform/OpenGL/OpenGLContext.cpp
+++ Birch/src/Platform/OpenGL/OpenGLContext.cpp
@@ -24,6 +24,11 @@ namespace Birch {
 		glfwMakeContextCurrent(m_windowHandle);
 		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		BC_CORE_ASSERTS(status, "Failed to initialize Glad!");
+
+		BC_CORE_INFO("OpenGL Info: ");
+		BC_CORE_INFO(" Vendor:{0}", glGetString(GL_VENDOR));
+		BC_CORE_INFO(" Renderer:{0}", glGetString(GL_RENDERER));
+		BC_CORE_INFO(" Version:{0}", glGetString(GL_VERSION));
 	}
 
 	void OpenGLContext::SwapBuffers()
