d56311c860787109e4b71d42fb0216db87844f4d
chuyan1 <379482455@qq.com>
2022-11-11 00:24:00 +0800

    上传思维导图

--- Birch/src/Birch/Renderer/Renderer2D.cpp
+++ Birch/src/Birch/Renderer/Renderer2D.cpp
@@ -12,7 +12,6 @@ namespace Birch {
 	struct Renderer2DStorage
 	{
 		Ref<VertexArray> QuadVertexArray;
-		//Ref<Shader> FlatColorShader; 
 		Ref<Shader> TextureShader;
 		Ref<Texture2D> WhiteTexture;
 	};
@@ -44,7 +43,6 @@ namespace Birch {
 		squareIB.reset(Birch::IndexBuffer::Create(squareIndices, sizeof(squareIndices) / sizeof(uint32_t)));
 		s_Data->QuadVertexArray->SetIndexBuffer(squareIB);
 
-		//s_Data->FlatColorShader = Shader::Create("assets/shaders/FlatColor.glsl");
 		s_Data->WhiteTexture = Texture2D::Create(1, 1);
 		uint32_t whiteTextureData = 0xffffffff;
 		s_Data->WhiteTexture->SetData(&whiteTextureData, sizeof(uint32_t));
@@ -61,9 +59,6 @@ namespace Birch {
 
 	void Renderer2D::BeginScene(OrthographicCamera& camera)
 	{		
-		//s_Data->FlatColorShader->Bind();
-		//s_Data->FlatColorShader->SetMat4("u_ViewProjection", camera.GetViewProjectionMatrix());
-
 		s_Data->TextureShader->Bind();
 		s_Data->TextureShader->SetMat4("u_ViewProjection", camera.GetViewProjectionMatrix());
 	}
@@ -80,14 +75,11 @@ namespace Birch {
 
 	void Renderer2D::DrawQuad(const glm::vec3& position, const glm::vec2& size, const glm::vec4& color)
 	{
-		//s_Data->FlatColorShader->Bind();
-		//s_Data->FlatColorShader->SetFloat4("u_Color", color);
-		s_Data->TextureShader->SetFloat4("u_Color", color);
+		s_Data->TextureShader->SetFloat4("u_Color", color);	// 纯色纹理
 		s_Data->WhiteTexture->Bind();
 
 		glm::mat4 transform = glm::translate(glm::mat4(1.0f), position) * glm::scale(glm::mat4(1.0f), { size.x, size.y, 1.0f });
 		
-		//s_Data->FlatColorShader->SetMat4("u_Transform", transform); 
 		s_Data->TextureShader->SetMat4("u_Transform", transform);
 
 		s_Data->QuadVertexArray->Bind();
@@ -101,8 +93,7 @@ namespace Birch {
 
 	void Renderer2D::DrawQuad(const glm::vec3& position, const glm::vec2& size, const Ref<Texture2D>& texture)
 	{
-		//s_Data->TextureShader->Bind(); 
-		s_Data->TextureShader->SetFloat4("u_Color", glm::vec4(0.5f, 0.7f, 0.6f, 1.0f));
+		s_Data->TextureShader->SetFloat4("u_Color", glm::vec4(0.5f, 0.7f, 0.6f, 1.0f));	// 图片纹理的底色
 		texture->Bind();
 
 		glm::mat4 transform = glm::translate(glm::mat4(1.0f), position) * glm::scale(glm::mat4(1.0f), { size.x, size.y, 1.0f });
--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -26,15 +26,15 @@ void Sandbox2D::OnUpdate(Birch::Timestep ts)
 	m_CameraController.OnUpdate(ts);
 
 	// Render
-	Birch::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
+	Birch::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });		// 设置背景颜色
 	Birch::RenderCommand::clear();
 
 	Birch::Renderer2D::BeginScene(m_CameraController.GetCamera());
 
-	Birch::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
+	Birch::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });		// 两个矩形的绘制
 	Birch::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, { 0.2f, 0.3f, 0.8f, 1.0f });
 
-	Birch::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture);
+	Birch::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture);	// 绘制棋盘格
 
 	Birch::Renderer2D::EndScene();
 }
@@ -42,7 +42,7 @@ void Sandbox2D::OnUpdate(Birch::Timestep ts)
 void Sandbox2D::OnImGuiRender()
 {
 	ImGui::Begin("Setting");
-	ImGui::ColorEdit3("Square Color", glm::value_ptr(m_SquareColor));
+	ImGui::ColorEdit3("Square Color", glm::value_ptr(m_SquareColor));	// 调色板颜色
 	ImGui::End();
 }
 
